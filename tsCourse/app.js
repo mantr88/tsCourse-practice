316ce53f5b366ddbbdd15ff783defeb27321e7ef
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-05-25 21:57:58 +0300

    lesson-2 exanples

--- tsCourse/app.js
+++ tsCourse/app.js
@@ -1,9 +1,86 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+var age;
+var toggle;
+var empty;
+var name1;
+var notInitialize;
+var callback = function (argg) { return number; };
+age = 50;
+toggle = true;
+empty = null;
+name1 = "Max";
+notInitialize = undefined;
+callback = function (a) {
+    return 100 + a;
+};
+// ================================================
+var anything;
+anything = -20;
+anything = "Text";
+anything = {};
+// ================================================
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+// ================================================
+var person;
+person = ["Max", 21];
+// ================================================
+var Stage;
+(function (Stage) {
+    Stage[Stage["LOADING"] = 0] = "LOADING";
+    Stage[Stage["READY"] = 1] = "READY";
+})(Stage || (Stage = {}));
+var filmStage = {
+    stageNow: Stage.READY,
+};
+if (filmStage.stageNow === Stage.READY) {
+    console.log("Film is allready loaded");
+}
+if (filmStage.stageNow === Stage.LOADING) {
+    console.log("A Film is still loading");
+}
+// ================================================
+var something;
+something = "1";
+// ================================================
+// 'enable' або 'disable'
+var acces;
+acces = "disable";
+// ================================================
+// function showMessage(message) {
+//   console.log(message);
+// }
+function showMessage(message) {
+    console.log(message);
+}
+// function calc(num1, num2) {
+//   return num1 + num2;
+// }
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+// function customError() {
+//   throw new Error("Error");
+// }
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -1,11 +1,118 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+let age: number;
+let toggle: boolean;
+let empty: null;
+let name1: string;
+let notInitialize: undefined;
+let callback = (argg: number) => number;
 
-function add(num1: number, num2: number) {
+age = 50;
+toggle = true;
+empty = null;
+name1 = "Max";
+notInitialize = undefined;
+callback = (a) => {
+  return 100 + a;
+};
+
+// ================================================
+let anything: any;
+
+anything = -20;
+anything = "Text";
+anything = {};
+
+// ================================================
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+}
+// ================================================
+let person: [string, number];
+
+person = ["Max", 21];
+
+// ================================================
+
+enum Stage {
+  LOADING,
+  READY,
+}
+
+const filmStage = {
+  stageNow: Stage.READY,
+};
+
+if (filmStage.stageNow === Stage.READY) {
+  console.log("Film is allready loaded");
+}
+if (filmStage.stageNow === Stage.LOADING) {
+  console.log("A Film is still loading");
+}
+// ================================================
+let something: string | number;
+
+something = "1";
+
+// ================================================
+// 'enable' або 'disable'
+
+let acces: "enable" | "disable";
+
+acces = "disable";
+// ================================================
+// function showMessage(message) {
+//   console.log(message);
+// }
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+// function calc(num1, num2) {
+//   return num1 + num2;
+// }
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+// function customError() {
+//   throw new Error("Error");
+// }
+
+function customError(): never {
+  throw new Error("Error");
+}
+// ================================================
+type PageType = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1: PageType = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
