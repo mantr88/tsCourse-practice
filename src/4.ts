7f3dd64c8180c893acfcb370fc94a087e9da04c8
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-09-27 11:42:36 +0300

    done task 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door = false;
+  key: Key;
+  tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  door = false;
+  key = new Key();
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  openDoor(key: Key): void {
+    if ((this.key = key)) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +58,12 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+const johnKey = new Key();
+const johnHouse = new MyHouse(johnKey);
+const john = new Person(johnKey);
+
+johnHouse.openDoor(john.getKey());
+
+johnHouse.comeIn(john);
 
 export {};
