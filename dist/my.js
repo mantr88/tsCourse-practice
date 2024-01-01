bcdf89e4517b39570459a6b51907da083c52c758
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2024-01-01 20:13:41 +0200

    add: init files

--- dist/my.js
+++
@@ -1,9 +0,0 @@
-function greet(person) {
-    console.log(`Hello, ${person.firstName} ${person.lastName}`);
-}
-const john = {
-    firstName: "John",
-    lastName: "Doe",
-};
-greet(john); // Виведе: "Hello, John Doe"
-//# sourceMappingURL=my.js.map
